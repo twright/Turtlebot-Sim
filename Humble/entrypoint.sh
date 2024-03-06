20cc309d18ff612432c84ffb3dc8931bee384a85
Thomas Wright <tom.tdw@gmail.com>
2024-03-06 18:34:51 +0100

    Initial version of dev environment

---
+++ Humble/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Basic entrypoint for ROS / Colcon Docker containers
+
+# Source ROS 2
+source /opt/ros/${ROS_DISTRO}/setup.bash
+# echo "Sourced ROS 2 ${ROS_DISTRO}"
+
+# Source the base workspace, if built
+if [ -f /opt/setup.bash ]
+then
+  source /opt/setup.bash
+  # echo "Sourced TurtleBot4 base workspace"
+fi
+
+# Execute the command passed into this entrypoint
+exec "$@"
