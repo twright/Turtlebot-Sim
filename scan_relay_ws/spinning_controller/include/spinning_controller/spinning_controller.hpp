d610df3e5a75f5d1848d5434a5f611eb27e96feb
Morten Haahr Kristensen <mhk@ece.au.dk>
2024-05-18 22:40:15 +0200

    Made the class general to any controller

--- scan_relay_ws/spinning_controller/include/spinning_controller/spinning_controller.hpp
+++ scan_relay_ws/spinning_controller/include/spinning_controller/spinning_controller.hpp
@@ -8,15 +8,13 @@
 #ifndef SPINNING_CONTROLLER__SPINNING_CONTROLLER_HPP_
 #define SPINNING_CONTROLLER__SPINNING_CONTROLLER_HPP_
 
-#include <string>
 #include <vector>
 #include <memory>
+#include <algorithm>
+#include <concepts>
 
 #include "nav2_core/controller.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "pluginlib/class_loader.hpp"
-#include "pluginlib/class_list_macros.hpp"
-#include "dwb_core/dwb_local_planner.hpp"
 
 namespace spinning_controller
 {
@@ -33,20 +31,78 @@ struct SpinInstructions
   double period;
 };
 
-// TODO: Template class - base class Controller
-class SpinningController : public dwb_core::DWBLocalPlanner
+template <typename T>
+concept is_nav2_controller = std::derived_from<T, nav2_core::Controller>;
+
+template <is_nav2_controller T>
+class SpinningController : public T
 {
 public:
   void configure(const rclcpp_lifecycle::LifecycleNode::WeakPtr& parent, std::string name,
                  std::shared_ptr<tf2_ros::Buffer> tf,
-                 std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+                 std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override
+  {
+    T::configure(parent, name, tf, costmap_ros);
+    auto node = parent.lock();
+    logger_ = node->get_logger();
+    clock_ = node->get_clock();
+    last_spin_ = clock_->now().seconds();
+    period_start_ = clock_->now().seconds();
+
+    std::vector<SpinInstructions::Instruction> instrcts{ { { 1.57, 1.0 }, { -1.57, 1.0 } } };
+    instructions_ = { std::move(instrcts), 5 };
+
+    RCLCPP_INFO(logger_, "SpinningController::configure");
+  }
 
   geometry_msgs::msg::TwistStamped computeVelocityCommands(const geometry_msgs::msg::PoseStamped& pose,
-                                                           const geometry_msgs::msg::Twist&,
-                                                           nav2_core::GoalChecker*) override;
+                                                           const geometry_msgs::msg::Twist& velocity,
+                                                           nav2_core::GoalChecker* goal_checker) override
+  {
+    auto cmd_vel = T::computeVelocityCommands(pose, velocity, goal_checker);
+    maybe_spin(cmd_vel);
+    return cmd_vel;
+  }
 
 private:
-  void maybe_spin(geometry_msgs::msg::TwistStamped& cmd_vel);
+  void maybe_spin(geometry_msgs::msg::TwistStamped& cmd_vel)
+  {
+    // TODO: Somewhere assert that sum of instruction durations is less than total duration
+    // using LinearVec3 = decltype(cmd_vel.twist.linear);
+
+    auto now = clock_->now().seconds();
+
+    // Check if we need to spin
+    for (size_t i = 0; i < instructions_.instructions.size(); i++)
+    {
+      auto end_it = instructions_.instructions.begin() + i + 1;
+      auto time_delta = std::accumulate(instructions_.instructions.begin(), end_it, 0.0,
+                                        [](auto res, auto instr) { return res + instr.duration; });
+      if (last_spin_ < period_start_ + time_delta)
+      {
+        last_spin_ = now;
+        cmd_vel.twist.linear.x = 0.0;
+        cmd_vel.twist.linear.y = 0.0;
+        cmd_vel.twist.linear.z = 0.0;
+
+        cmd_vel.twist.angular.z = instructions_.instructions[i].omega;
+        RCLCPP_INFO(logger_, "Spinning: %f, %f, %f, %f", last_spin_, time_delta, period_start_,
+                    instructions_.instructions[i].duration);
+        return;
+      }
+      else
+      {
+        RCLCPP_INFO(logger_, "Not spinning: %f, %f, %f, %f", last_spin_, time_delta, period_start_,
+                    instructions_.instructions[i].duration);
+      }
+    }
+    if (period_start_ + instructions_.period < now)
+    {
+      RCLCPP_INFO(logger_, "New period");
+      period_start_ = now;
+      last_spin_ = now;
+    }
+  }
 
   rclcpp::Logger logger_{ rclcpp::get_logger("SpinningController") };
   rclcpp::Clock::SharedPtr clock_;
--- scan_relay_ws/spinning_controller/src/spinning_controller.cpp
+++ scan_relay_ws/spinning_controller/src/spinning_controller.cpp
@@ -6,77 +6,23 @@
  */
 
 #include "spinning_controller/spinning_controller.hpp"
-#include <algorithm>
+#include "pluginlib/class_loader.hpp"
+#include "pluginlib/class_list_macros.hpp"
+
+// Controller specializations
+#include "dwb_core/dwb_local_planner.hpp"
+// #include "nav2_regulated_pure_pursuit_controller/regulated_pure_pursuit_controller.hpp"
 
 namespace spinning_controller
 {
+using DWBSpinningController = spinning_controller::SpinningController<dwb_core::DWBLocalPlanner>;
 
-void SpinningController::maybe_spin(geometry_msgs::msg::TwistStamped& cmd_vel)
-{
-  // TODO: Somewhere assert that sum of instruction durations is less than total duration
-  // using LinearVec3 = decltype(cmd_vel.twist.linear);
-
-  auto now = clock_->now().seconds();
-
-  // Check if we need to spin
-  for (size_t i = 0; i < instructions_.instructions.size(); i++)
-  {
-    auto end_it = instructions_.instructions.begin() + i + 1;
-    auto time_delta = std::accumulate(instructions_.instructions.begin(), end_it, 0.0,
-                                      [](auto res, auto instr) { return res + instr.duration; });
-    if (last_spin_ < period_start_ + time_delta)
-    {
-      last_spin_ = now;
-      cmd_vel.twist.linear.x = 0.0;
-      cmd_vel.twist.linear.y = 0.0;
-      cmd_vel.twist.linear.z = 0.0;
-
-      cmd_vel.twist.angular.z = instructions_.instructions[i].omega;
-      RCLCPP_INFO(logger_, "Spinning: %f, %f, %f, %f", last_spin_, time_delta, period_start_,
-                  instructions_.instructions[i].duration);
-      return;
-    }
-    else
-    {
-      RCLCPP_INFO(logger_, "Not spinning: %f, %f, %f, %f", last_spin_, time_delta, period_start_,
-                  instructions_.instructions[i].duration);
-    }
-  }
-  if (period_start_ + instructions_.period < now)
-  {
-    RCLCPP_INFO(logger_, "New period");
-    period_start_ = now;
-    last_spin_ = now;
-  }
-}
-
-void SpinningController::configure(const rclcpp_lifecycle::LifecycleNode::WeakPtr& parent, std::string name,
-                                   std::shared_ptr<tf2_ros::Buffer> tf,
-                                   std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros)
-{
-  dwb_core::DWBLocalPlanner::configure(parent, name, tf, costmap_ros);
-  auto node = parent.lock();
-  logger_ = node->get_logger();
-  clock_ = node->get_clock();
-  last_spin_ = clock_->now().seconds();
-  period_start_ = clock_->now().seconds();
-
-  std::vector<SpinInstructions::Instruction> instrcts{ { { 1.57, 1.0 }, { -1.57, 1.0 } } };
-  instructions_ = { std::move(instrcts), 5 };
-
-  RCLCPP_INFO(logger_, "SpinningController::configure");
-}
-
-geometry_msgs::msg::TwistStamped SpinningController::computeVelocityCommands(
-    const geometry_msgs::msg::PoseStamped& pose, const geometry_msgs::msg::Twist& velocity,
-    nav2_core::GoalChecker* goal_checker)
-{
-  auto cmd_vel = dwb_core::DWBLocalPlanner::computeVelocityCommands(pose, velocity, goal_checker);
-  maybe_spin(cmd_vel);
-  return cmd_vel;
-}
+// Alternative controller? Simply comment in code, change nav2_params and add CMake dependencies
+// using RegulatedSpinningPurePursuitController =
+//     spinning_controller::SpinningController<nav2_regulated_pure_pursuit_controller::RegulatedPurePursuitController>;
 
 }  // namespace spinning_controller
 
-// Register this controller as a nav2_core plugin
-PLUGINLIB_EXPORT_CLASS(spinning_controller::SpinningController, nav2_core::Controller)
+// Register the controllers as a nav2_core plugin
+PLUGINLIB_EXPORT_CLASS(spinning_controller::DWBSpinningController, nav2_core::Controller)
+// PLUGINLIB_EXPORT_CLASS(spinning_controller::RegulatedSpinningPurePursuitController, nav2_core::Controller)
