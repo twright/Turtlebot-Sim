aabce62b126c1352909a4ca28cb40ac7fa9a4a8d
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2024-06-06 11:00:10 +0200

    Fixed bug with scan_modifier and minimum_angle

--- scan_relay_ws/scan_modifier/src/scan_node.cpp
+++ scan_relay_ws/scan_modifier/src/scan_node.cpp
@@ -55,27 +55,25 @@ void ScanModifierNode::scan_sub_callback(const sensor_msgs::msg::LaserScan::Shar
   const auto& logger = get_logger();
   auto level = rcutils_logging_get_logger_effective_level(logger.get_name());
 
-  RCLCPP_INFO(get_logger(), "Callback");
-
   if (level == RCUTILS_LOG_SEVERITY_DEBUG)
   {
     RCLCPP_DEBUG(get_logger(), "OG msg:");
     print_laser(*laser_msg, get_logger());
   }
 
-  RCLCPP_INFO(get_logger(), "OG sizes: %ld, %ld", laser_msg->ranges.size(), laser_msg->intensities.size());
+  RCLCPP_DEBUG(get_logger(), "OG sizes: %ld, %ld", laser_msg->ranges.size(), laser_msg->intensities.size());
+  RCLCPP_DEBUG(get_logger(), "Scan config: %d, %d", lidar_filt_lower_, lidar_filt_upper_);
 
   auto laser_size = laser_msg->ranges.size();
-  laser_msg->angle_min = lidar_filt_lower_ / laser_size * 2 * M_PI;
-  laser_msg->angle_max = lidar_filt_upper_ / laser_size * 2 * M_PI;
+  laser_msg->angle_min = static_cast<float>(lidar_filt_lower_) / laser_size * 2 * M_PI;
+  laser_msg->angle_max = static_cast<float>(lidar_filt_upper_) / laser_size * 2 * M_PI;
   auto result = calculate_lidar_ranges(lidar_filt_lower_, lidar_filt_upper_, laser_msg->ranges);
 
-  // TODO: Probably bug in the resizes...
-  laser_msg->ranges.resize(result.size());
+  // TODO: Intensities are being incorrectly configured but since it is unused, it is OK for now
   laser_msg->intensities.resize(result.size());
 
   laser_msg->ranges = std::move(result);
-  RCLCPP_INFO(get_logger(), "New sizes: %ld, %ld", laser_msg->ranges.size(), laser_msg->intensities.size());
+  RCLCPP_DEBUG(get_logger(), "New sizes: %ld, %ld", laser_msg->ranges.size(), laser_msg->intensities.size());
 
   if (level == RCUTILS_LOG_SEVERITY_DEBUG)
   {
